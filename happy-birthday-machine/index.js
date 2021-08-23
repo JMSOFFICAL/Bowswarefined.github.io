8a347804178e56a5001a9b60e4f9250229b88242
Bowswa <69088845+Bowswa@users.noreply.github.com>
2021-08-23 15:10:31 -0400

    Push These Changes:
    -Multi Person Support Pt1
    -Fix "Use Default Colors Button"
    -Backend Optimizations(re-wrote submit function, split files up)

--- happy-birthday-machine/index.js
+++ happy-birthday-machine/index.js
@@ -1,4 +1,5 @@
 function setCake() {
+  //close
     var e = document.getElementById("cakes");
     var value = e.options[e.selectedIndex].value;
     localStorage.setItem("cake", value);
@@ -18,131 +19,137 @@ function setCake() {
 };
 
 function submit() {
-    var color1 = document.getElementById('color1').value;
-    var color2 = document.getElementById('color2').value;
-    var font = document.getElementById('font-in').value;
+  var requiredValues = 0;
+  //names
+  var name1 = document.getElementById('user').value;
+  var name2 = document.getElementById('user2').value;
+  var name3 = document.getElementById('user3').value;
 
-    var name = document.getElementById('user').value;
+  if (!name1) {
+    return alert('You must enter a name!');
+  }else if (name1) {
+    requiredValues = requiredValues + 1;
+  }else {
+    alert('Something went wrong. Please check the console.');
+    console.log('Please join the discord server and report this error: ERROR CODE 11111-0: LN 34.js')
+  }
+  //colors
+  var color1 = document.getElementById('color1').value;
+  var color2 = document.getElementById('color2').value;
 
-    var requiredValues = 0;
-    if (!name) {
-        alert('You must enter a name!');
-    } else if (name) {
-        requiredValues = requiredValues + 1;
-    } else {
-        alert('Unknown error!');
+  var defaultColorCount = 0;
+  
+  if (color1 = '#00FF33') {
+    defaultColorCount = defaultColorCount + 1;
+  }else if (!(color1 = '#00FF33')) {
+    requiredValues = requiredValues + 1;
+  }
+
+  if (color2 = '#0091FF') {
+    defaultColorCount = defaultColorCount + 1;
+  }else if (!(color2 = '#0091FF')) {
+    requiredValues = requiredValues + 1;
+  }
+
+  if (defaultColorCount = 2) {
+    defaultColorConfirm = confirm('You are using the default colors. Are you sure you want to do this?');
+    if (defaultColorConfirm === true) {
+      requiredValues = requiredValues + 2;
+    }else if (defaultColorConfirm === false) {
+      return alert('You have decided to use custom colors. Submit again if this was a mistake!');
     }
-
-    if (color1 = '#00FF33') {
-        if (color2 = '#0091FF') {
-            colorConfirm = confirm('You are using the default colors. Are you sure you want to do this?');
-            if (colorConfirm === true) {
-                requiredValues = requiredValues + 1;
-            } else if (colorConfirm === false) {
-                return alert('You have decided to use custom colors. Submit again if this was a mistake!');
-            }
-        } else return;
-    } else if (color1 && color2) {
-        requiredValues = requiredValues + 1;
+  }else return;
+  //font
+  var font = document.getElementById('font-in').value;
+  //submit all
+  if (requiredValues < 3) {
+    return;
+  } else if (requiredValues >= 3) {
+    localStorage.setItem('BdayPerson', name1);
+    if (name2) {
+      localStorage.setItem('BdayPerson2', name2);
     }
-
-    if (requiredValues < 2) {
-        return;
-    } else if (requiredValues >= 2) {
-        localStorage.setItem('BdayPerson', name);
-        localStorage.setItem('color1', color1);
-        localStorage.setItem('color2', color2);
-        if (font) {
-            localStorage.setItem('font', font);
-        }
-        location.replace('http://adfoc.us/53726676042623'); //ad link.
+    if (name3) {
+      localStorage.setItem('BdayPerson3', name3);
     }
+    localStorage.setItem('color1', color1);
+    localStorage.setItem('color2', color2);
+    if (font) {
+      localStorage.setItem('font', font);
+    }
+    location.replace('http://adfoc.us/53726676042623'); //ad link.
+  }else {
+    alert('Something Went Wrong! Please Check Console!');
+    console.log('Please join the discord server and report this error: ERROR CODE 22222-0: LN 77.js');
+  }
 };
 
 function dirSubmit() {
-    var color1 = document.getElementById('color1').value;
-    var color2 = document.getElementById('color2').value;
-    var font = document.getElementById('font-in').value;
+  var requiredValues = 0;
+  //names
+  var name1 = document.getElementById('user').value;
+  var name2 = document.getElementById('user2').value;
+  var name3 = document.getElementById('user3').value;
 
-    var name = document.getElementById('user').value;
+  if (!name1) {
+    return alert('You must enter a name!');
+  }else if (name1) {
+    requiredValues = requiredValues + 1;
+  }else {
+    alert('Something went wrong. Please check the console.');
+    console.log('Please join the discord server and report this error: ERROR CODE 11111-1: LN 94.js')
+  }
+  //colors
+  var color1 = document.getElementById('color1').value;
+  var color2 = document.getElementById('color2').value;
 
-    var requiredValues = 0;
-    if (!name) {
-        alert('You must enter a name!');
-    } else if (name) {
-        requiredValues = requiredValues + 1;
-    } else {
-        alert('Unknown error!');
+  var defaultColorCount = 0;
+  
+  if (color1 = '#00FF33') {
+    defaultColorCount = defaultColorCount + 1;
+  }else if (!(color1 = '#00FF33')) {
+    requiredValues = requiredValues + 1;
+  }
+
+  if (color2 = '#0091FF') {
+    defaultColorCount = defaultColorCount + 1;
+  }else if (!(color2 = '#0091FF')) {
+    requiredValues = requiredValues + 1;
+  }
+
+  if (defaultColorCount = 2) {
+    defaultColorConfirm = confirm('You are using the default colors. Are you sure you want to do this?');
+    if (defaultColorConfirm === true) {
+      requiredValues = requiredValues + 2;
+    }else if (defaultColorConfirm === false) {
+      return alert('You have decided to use custom colors. Submit again if this was a mistake!');
     }
-
-    if (color1 = '#00FF33') {
-        if (color2 = '#0091FF') {
-            colorConfirm = confirm('You are using the default colors. Are you sure you want to do this?');
-            if (colorConfirm === true) {
-                requiredValues = requiredValues + 1;
-            } else if (colorConfirm === false) {
-                return alert('You have decided to use custom colors. Submit again if this was a mistake!');
-            }
-        } else return;
-    } else if (color1 && color2) {
-        requiredValues = requiredValues + 1;
+  }else return;
+  //font
+  var font = document.getElementById('font-in').value;
+  //submit all
+  if (requiredValues < 3) {
+    return;
+  } else if (requiredValues >= 3) {
+    localStorage.setItem('BdayPerson', name1);
+    if (name2) {
+      localStorage.setItem('BdayPerson2', name2);
     }
-
-    if (requiredValues < 2) {
-        return;
-    } else if (requiredValues >= 2) {
-        localStorage.setItem('BdayPerson', name);
-        localStorage.setItem('color1', color1);
-        localStorage.setItem('color2', color2);
-        if (font) {
-            localStorage.setItem('font', font);
-        }
-        location.replace('birthday.html');
+    if (name3) {
+      localStorage.setItem('BdayPerson3', name3);
     }
+    localStorage.setItem('color1', color1);
+    localStorage.setItem('color2', color2);
+    if (font) {
+      localStorage.setItem('font', font);
+    }
+    location.replace('birthday.html'); //direct link
+  }else {
+    alert('Something Went Wrong! Please Check Console!');
+    console.log('Please join the discord server and report this error: ERROR CODE 22222-1: LN 137.js');
+  }
 };
-
-/*function submit() {
-var color1 = document.getElementById('color1').value;
-var color2 = document.getElementById('color2').value;
-var font = document.getElementById('font-in').value;
-localStorage.removeItem('BdayPerson');
-localStorage.removeItem('color1');
-localStorage.removeItem('color2');
-localStorage.setItem('color1', color1);
-localStorage.setItem('color2', color2);
-localStorage.removeItem('font');
-localStorage.setItem('font', font);
-var name = document.getElementById('user').value;
-if (!name) {
-  return alert('You must enter a name!');
-} else if (name) {
-  localStorage.setItem('BdayPerson', name);
-} else {
-  return alert('Unknown error. If this keeps happening please report on my discord!')
-}
-if (!color1 || !color2) {
-  if (!color1S || !color2S) {
-    return alert('You must choose both colors!');
-  }
-} else if (color1S && color2S) {
-  savedColor = confirm('You are using previously saved colors. Do you still want to? Press "Cancel" to set new colors');
-  if (savedColor === true) {} 
-  else if (savedColor === false) {
-    return alert('You decided to set custom colors. Submit again if this was a mistake!');
-  }
-} else if (color1 && color2) {
-  localStorage.setItem('color1', color1);
-  localStorage.setItem('color2', color2);
-} else {
-  return alert('Unknown error. If this keeps happening please report on my discord!')
-}
-if (!localStorage.getItem('BdayPerson')) {
-  return;
-} else {
-  location.replace('birthday.html');
-}
-};*/
 function setDefaultColor() {
-    document.getElementById('color1').setAttribute('value', '#00FF33');
-    document.getElementById('color2').setAttribute('value', '#0091FF');
+    document.getElementById('color1').value = '#00FF33'
+    document.getElementById('color2').value = '#0091FF'
 };
